4a443075a847761d8395147359fd2ffe52e2ff16
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-09-11 14:56:35 -0300

    Peer nodes in compose.

--- src/configurations/generateDockerCompose.ts
+++ src/configurations/generateDockerCompose.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { BLUE, RESET } from "../utils/colors";
 import superNodesConfiguration from "./super-nodes.json";
+import peerNodesConfiguration from "./peer-nodes.json";
 
 let dockerComposeContent = "";
 dockerComposeContent += 'version: "3.9"\n';
@@ -12,14 +13,29 @@ for (let i = 0; i < superNodesConfiguration.nodes.length; i++) {
   if (!["127.0.0.1", "localhost"].includes(node.address.toLowerCase())) {
     continue;
   }
-  dockerComposeContent += `  ${node.name}:\n`;
-  dockerComposeContent += `    container_name: ${node.name}\n`;
+  dockerComposeContent += `  super-node-${i + 1}:\n`;
+  dockerComposeContent += `    container_name: super-node-${i + 1}\n`;
   dockerComposeContent += "    environment:\n";
   dockerComposeContent += `      - NODE_NAME=${node.name}\n`;
   dockerComposeContent += "    ports:\n";
   dockerComposeContent += `      - ${node.port}:${node.port}\n`;
   dockerComposeContent += "    build: .\n";
-  dockerComposeContent += "    entrypoint: npm run start-supernode-container\n";
+  dockerComposeContent += "    entrypoint: npm run super-node-container\n";
+}
+
+dockerComposeContent += "  # Peer Nodes.\n";
+for (let i = 0; i < peerNodesConfiguration.nodes.length; i++) {
+  const node = peerNodesConfiguration.nodes[i];
+  dockerComposeContent += `  peer-node-${i + 1}:\n`;
+  dockerComposeContent += `    container_name: peer-node-${i + 1}\n`;
+  dockerComposeContent += "    environment:\n";
+  dockerComposeContent += `      - NODE_NAME=${node.name}\n`;
+  dockerComposeContent += `      - NODE_PORT=${node.port}\n`;
+  dockerComposeContent += `      - RESOURCES_DIRECTORY=${node.resources_directory}\n`;
+  dockerComposeContent += "    ports:\n";
+  dockerComposeContent += `      - ${node.port}:${node.port}\n`;
+  dockerComposeContent += "    build: .\n";
+  dockerComposeContent += "    entrypoint: npm run peer-node-container\n";
 }
 
 try {
